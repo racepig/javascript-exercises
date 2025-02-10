8864dd4ff24bf8caa7a3b2cab0a6e66397783e07
racepig <84897520+racepig@users.noreply.github.com>
2025-02-10 13:47:59 +0900

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("")
+    arr.reverse()
+    let result = arr.join("")
+    return result
 };
 
 // Do not edit below this line
