99846591a70e693d6a6f2c834d29caf4bddcd479
racepig <84897520+racepig@users.noreply.github.com>
2025-02-10 13:21:25 +0900

    finished exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    let i = 0;
+    let result = "";
+    while (i < num){
+        console.log(str);
+        result += str;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
